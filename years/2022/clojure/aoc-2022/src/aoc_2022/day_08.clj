b93e912d168e61d36393b5374de3baae8ddd4dc5
Craig S. Cottingham <craig@cottingham.net>
2022-12-08 17:07:55 -0600

    feat: update files for day 08 part 2

--- years/2022/clojure/aoc-2022/src/aoc_2022/day_08.clj
+++ years/2022/clojure/aoc-2022/src/aoc_2022/day_08.clj
@@ -61,18 +61,81 @@
   [grid row-num col-num]
   (some (fn [f] (f grid row-num col-num)) (list is-visible-from-north? is-visible-from-east? is-visible-from-south? is-visible-from-west?)))
 
+;; functions for measuring viewing distance
+
+(defn viewing-distance-along-seq
+  "Determines the viewing distance using the supplied sequence of coordinates.
+   The viewing distance is the number of trees to the first one which has a height the same as or greater than the current height.
+   The distance ends up being 0-based, so we need to increment it before returning it."
+  [grid this-height direction-seq]
+  (let [short-tree-seq (keep-indexed (fn [idx [other-row-num other-col-num]]
+                                       (when (>= (matrix/mget grid other-row-num other-col-num) this-height) idx)) direction-seq)]
+    (if (seq short-tree-seq)
+      (inc (first short-tree-seq))
+      (count direction-seq))))
+
+(defn viewing-distance-to-east
+  "Determines the viewing distance to the east (toward the right side of the grid)."
+  [grid row-num col-num]
+  (let [[_row-count col-count] (matrix/shape grid)
+        this-height (matrix/mget grid row-num col-num)
+        start-col-num (inc col-num)
+        direction-seq (map (fn [other-col-num] (vector row-num other-col-num)) (range start-col-num col-count))]
+    (if (= start-col-num col-count)
+      0
+      (viewing-distance-along-seq grid this-height direction-seq))))
+
+(defn viewing-distance-to-north
+  "Determines the viewing distance to the north (toward the top side of the grid)."
+  [grid row-num col-num]
+  (let [[_row-count _col-count] (matrix/shape grid)
+        this-height (matrix/mget grid row-num col-num)
+        start-row-num (dec row-num)
+        direction-seq (map (fn [other-row-num] (vector other-row-num col-num)) (range start-row-num -1 -1))]
+    (if (= start-row-num -1)
+      0
+      (viewing-distance-along-seq grid this-height direction-seq))))
+
+(defn viewing-distance-to-south
+  "Determines the viewing distance to the south (toward the bottom side of the grid)."
+  [grid row-num col-num]
+  (let [[row-count _col-count] (matrix/shape grid)
+        this-height (matrix/mget grid row-num col-num)
+        start-row-num (inc row-num)
+        direction-seq (map (fn [other-row-num] (vector other-row-num col-num)) (range start-row-num row-count))]
+    (if (= start-row-num row-count)
+      0
+      (viewing-distance-along-seq grid this-height direction-seq))))
+
+(defn viewing-distance-to-west
+  "Determines the viewing distance to the west (toward the left side of the grid)."
+  [grid row-num col-num]
+  (let [[_row-count _col-count] (matrix/shape grid)
+        this-height (matrix/mget grid row-num col-num)
+        start-col-num (dec col-num)
+        direction-seq (map (fn [other-col-num] (vector row-num other-col-num)) (range start-col-num -1 -1))]
+    (if (= start-col-num -1)
+      0
+      (viewing-distance-along-seq grid this-height direction-seq))))
+
+(defn scenic-score
+  "Calculates the scenic score for a tree.
+   The scenic score is the product of the viewing distances in the cardinal directions from the tree."
+  [grid row-num col-num]
+  (apply * (map (fn [f] (f grid row-num col-num)) (list viewing-distance-to-north viewing-distance-to-east viewing-distance-to-south viewing-distance-to-west))))
+
 (defn part1 [filename]
   (let [grid (as-grid (read-file filename))
         [row-count col-count] (matrix/shape grid)
         row-col-seq (for [row-num (range row-count) col-num (range col-count)] [row-num col-num])]
     (count (filter (fn [[row-num col-num]] (is-visible? grid row-num col-num)) row-col-seq))))
 
-;; (defn part2 [filename]
-;;   (let [sizes (du (second (read-listing (read-file filename))))
-;;         total-used (first sizes)
-;;         needed (space-to-free total-used)]
-;;     (apply min (filter (fn [size] (>= size needed)) sizes))))
+(defn part2 [filename]
+  (let [grid (as-grid (read-file filename))
+        [row-count col-count] (matrix/shape grid)
+        row-col-seq (for [row-num (range row-count) col-num (range col-count)] [row-num col-num])]
+    (apply max (map (fn [[row-num col-num]] (scenic-score grid row-num col-num)) row-col-seq))))
 
 (defn -main []
-  (printf "day 08 part 1: %d%n" (part1 "../../data/08.txt")))
-  ;; (printf "day 08 part 2: %d%n" (part2 "../../data/08.txt")))
+  (printf "day 08 part 1: %d%n" (part1 "../../data/08.txt"))
+  (printf "day 08 part 2: %d%n" (part2 "../../data/08.txt")))
--- years/2022/clojure/aoc-2022/test/aoc_2022/day_08_test.clj
+++ years/2022/clojure/aoc-2022/test/aoc_2022/day_08_test.clj
@@ -4,8 +4,8 @@
 
 (deftest day-08-test
   (testing "sample data"
-    (is (= 21 (part1 "../../data/08-sample.txt"))))
-    ;; (is (= 24933642 (part2 "../../data/08-sample.txt"))))
+    (is (= 21 (part1 "../../data/08-sample.txt")))
+    (is (= 8 (part2 "../../data/08-sample.txt"))))
   (testing "puzzle data"
-    (is (= 1859 (part1 "../../data/08.txt")))))
-  ;;   (is (= 272298 (part2 "../../data/08.txt")))))
+    (is (= 1859 (part1 "../../data/08.txt")))
+    (is (= 332640 (part2 "../../data/08.txt")))))
