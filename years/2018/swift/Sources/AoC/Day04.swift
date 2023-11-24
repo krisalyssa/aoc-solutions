44e3b7345772891198b2e34fd829e4f1b65441c9
Craig S. Cottingham <craig@cottingham.net>
2023-11-24 00:21:04 -0600

    feat: add code for Swift 2018 day 04

---
+++ years/2018/swift/Sources/AoC/Day04.swift
@@ -0,0 +1,104 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
+import Algorithms
+import Collections
+import Common
+import Foundation
+
+public class Day04: Day {
+  typealias Guards = [Int: CountingSet<Int>]
+
+  public init() {}
+
+  public func part1(_ input: Input) {
+    let data = input.asStringArray().sorted()
+
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    data.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    id = Day04.sleepiestGuard(guards: guards)
+    let minute = Day04.sleepiestMinuteForGuard(timeslots: guards[id]!)
+
+    print("day 04 part 1: \(id * minute)")
+  }
+
+  public func part2(_ input: Input) {
+    let data = input.asStringArray().sorted()
+
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    data.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    let v = (0..<60).map { m in
+      let g = Day04.sleepiestGuardForMinute(guards: guards, minute: m)
+      return (g.0, m, g.1)
+    }.max { a, b in a.2 < b.2 }!
+
+    print("day 04 part 2: \(v.0 * v.1)")
+  }
+
+  static let regexBegin = #/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] Guard #(\d+)/#
+  static let regexSleep = #/\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})\] falls/#
+  static let regexWake = #/\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})\] wakes/#
+
+  static func minutesSleeping(_ timeslots: CountingSet<Int>) -> Int {
+    timeslots.values.reduce(0, +)
+  }
+
+  static func parseLine(_ line: String, id: inout Int, sleep: inout Int, guards: inout Guards) {
+    if let match = line.firstMatch(of: regexBegin) {
+      id = Int(match.1)!
+      return
+    }
+
+    if let match = line.firstMatch(of: regexSleep) {
+      sleep = Int(match.1)!
+      return
+    }
+
+    if let match = line.firstMatch(of: regexWake) {
+      for ts in sleep..<Int(match.1)! {
+        guards[id, default: CountingSet<Int>(minimumCapacity: 60)][ts, default: 0] += 1
+      }
+    }
+  }
+
+  static func sleepiestGuard(guards: [Int: CountingSet<Int>]) -> Int {
+    let minutesSlept = guards.map { (id, timeslots) in
+      return (id, Day04.minutesSleeping(timeslots))
+    }
+
+    let (id, _) = minutesSlept.max { a, b in a.1 < b.1 }!
+    return id
+  }
+
+  static func sleepiestGuardForMinute(guards: [Int: CountingSet<Int>], minute: Int) -> (Int, Int) {
+    guards.map { (id, timeslots) in
+      (id, timeslots[minute] ?? 0)
+    }.max { a, b in
+      a.1 < b.1
+    }!
+  }
+
+  static func sleepiestMinuteForGuard(timeslots: CountingSet<Int>) -> Int {
+    let (minute, _) = timeslots.max { a, b in a.1 < b.1 }!
+    return minute
+  }
+}
---
+++ years/2018/swift/Tests/AoCTests/TestDay04.swift
@@ -0,0 +1,127 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
+import Collections
+import XCTest
+
+@testable import AoC
+
+class TestDay04: XCTestCase {
+  let lines = [
+    "[1518-11-01 00:00] Guard #10 begins shift",
+    "[1518-11-01 00:05] falls asleep",
+    "[1518-11-01 00:25] wakes up",
+    "[1518-11-01 00:30] falls asleep",
+    "[1518-11-01 00:55] wakes up",
+    "[1518-11-01 23:58] Guard #99 begins shift",
+    "[1518-11-02 00:40] falls asleep",
+    "[1518-11-02 00:50] wakes up",
+    "[1518-11-03 00:05] Guard #10 begins shift",
+    "[1518-11-03 00:24] falls asleep",
+    "[1518-11-03 00:29] wakes up",
+    "[1518-11-04 00:02] Guard #99 begins shift",
+    "[1518-11-04 00:36] falls asleep",
+    "[1518-11-04 00:46] wakes up",
+    "[1518-11-05 00:03] Guard #99 begins shift",
+    "[1518-11-05 00:45] falls asleep",
+    "[1518-11-05 00:55] wakes up",
+  ]
+
+  func testMinutesSleeping() throws {
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    lines.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    XCTAssertEqual(Day04.minutesSleeping(guards[10]!), 50)
+    XCTAssertEqual(Day04.minutesSleeping(guards[99]!), 30)
+  }
+
+  func testParseLine() throws {
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    Day04.parseLine(
+      "[1518-11-01 00:00] Guard #10 begins shift", id: &id, sleep: &start, guards: &guards)
+    XCTAssertEqual(id, 10)
+    XCTAssertEqual(start, 0)
+    XCTAssertNil(guards[id])
+
+    Day04.parseLine(
+      "[1518-11-01 00:05] falls asleep", id: &id, sleep: &start, guards: &guards)
+    XCTAssertEqual(id, 10)
+    XCTAssertEqual(start, 5)
+    XCTAssertNil(guards[id])
+
+    Day04.parseLine(
+      "[1518-11-01 00:25] wakes up", id: &id, sleep: &start, guards: &guards)
+    XCTAssertEqual(id, 10)
+    XCTAssertEqual(start, 5)
+    XCTAssertNotNil(guards[id])
+    XCTAssertNil(guards[id]![4])
+    XCTAssertEqual(guards[id]![5], 1)
+    XCTAssertEqual(guards[id]![6], 1)
+    XCTAssertEqual(guards[id]![23], 1)
+    XCTAssertEqual(guards[id]![24], 1)
+    XCTAssertNil(guards[id]![25])
+
+    Day04.parseLine(
+      "[1518-11-03 00:05] Guard #10 begins shift", id: &id, sleep: &start, guards: &guards)
+    Day04.parseLine(
+      "[1518-11-03 00:24] falls asleep", id: &id, sleep: &start, guards: &guards)
+    Day04.parseLine(
+      "[1518-11-03 00:29] wakes up", id: &id, sleep: &start, guards: &guards)
+    XCTAssertEqual(guards[id]![23], 1)
+    XCTAssertEqual(guards[id]![24], 2)
+    XCTAssertEqual(guards[id]![25], 1)
+  }
+
+  func testSleepiestGuard() throws {
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    lines.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    XCTAssertEqual(Day04.sleepiestGuard(guards: guards), 10)
+  }
+
+  func testSleepiestGuardForMinute() throws {
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    lines.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    let (g, count) = Day04.sleepiestGuardForMinute(guards: guards, minute: 45)
+    XCTAssertEqual(g, 99)
+    XCTAssertEqual(count, 3)
+  }
+
+  func testSleepiestMinuteForGuard() throws {
+    var id = 0
+    var start = 0
+    var guards: [Int: CountingSet<Int>] = [:]
+
+    lines.forEach {
+      Day04.parseLine($0, id: &id, sleep: &start, guards: &guards)
+    }
+
+    XCTAssertEqual(Day04.sleepiestMinuteForGuard(timeslots: guards[10]!), 24)
+  }
+}
