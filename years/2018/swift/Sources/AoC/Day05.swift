1c09cfb210584bf1117bed4b3e18b1d1fca123a0
Craig S. Cottingham <craig@cottingham.net>
2023-11-24 17:17:13 -0600

    feat: add code for Swift 2018 day 05

---
+++ years/2018/swift/Sources/AoC/Day05.swift
@@ -0,0 +1,58 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
+import Algorithms
+import Collections
+import Common
+import Foundation
+
+public class Day05: Day {
+  public init() {}
+
+  public func part1(_ input: Input) {
+    let data = input.asCharacterArray().filter { $0 != "\n" }
+
+    print("day 05 part 1: \(String(Day05.react(polymer: data)).count)")
+  }
+
+  public func part2(_ input: Input) {
+    let data = input.asCharacterArray().filter { $0 != "\n" }
+
+    let shortest = Array("abcdefghijklmnopqrstuvwxyz").map { c in
+      Day05.react(polymer: data, skipping: Character(String(c))).count
+    }.min { a, b in a < b }!
+
+    print("day 05 part 2: \(shortest)")
+  }
+
+  static func isReactivePair(lhs: Character?, rhs: Character?) -> Bool {
+    if lhs == nil || rhs == nil { return false }
+
+    return (lhs!.lowercased() == rhs!.lowercased())
+      && ((lhs!.isLowercase && rhs!.isUppercase)
+        || (lhs!.isUppercase && rhs!.isLowercase))
+  }
+
+  static func react(polymer: [Character], skipping: Character? = nil) -> [Character] {
+    var reacted: [Character] = []
+
+    for i in 0..<polymer.count {
+      if (skipping == nil) || (skipping!.lowercased() != polymer[i].lowercased()) {
+        if isReactivePair(lhs: polymer[i], rhs: reacted.last) {
+          let _ = reacted.popLast()
+        } else {
+          reacted.append(polymer[i])
+        }
+      }
+    }
+
+    return reacted
+  }
+}
