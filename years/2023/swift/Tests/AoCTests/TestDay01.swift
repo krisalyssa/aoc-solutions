746f2b2bb89cafcaed444dcc1621d776a114c07b
Craig S. Cottingham <craig@cottingham.net>
2023-12-01 14:37:46 -0600

    chore: initial commit for 2023-swift

---
+++ years/2023/swift/Tests/AoCTests/TestDay01.swift
@@ -0,0 +1,19 @@
+/******************************************************************************
+ **
+ ** Copyright (c) 2023 Craig S. Cottingham
+ ** Licensed under the MIT License.
+ **
+ ** See https://github.com/CraigCottingham/swift-aoc-common/blob/main/LICENSE
+ ** for license information.
+ **
+ **/
+
+import XCTest
+
+@testable import AoC
+
+class TestDay01: XCTestCase {
+  func testSomeFunc() async throws {
+    XCTAssertEqual(1, 1)
+  }
+}
