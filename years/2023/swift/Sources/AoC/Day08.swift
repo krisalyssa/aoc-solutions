558896a9627aa1e5d9677e2d28e73f89710f527e
Craig S. Cottingham <craig@cottingham.net>
2023-12-17 17:04:19 -0600

    feat: add code for Swift 2023 day 08 part 2

--- years/2023/swift/Sources/AoC/Day08.swift
+++ years/2023/swift/Sources/AoC/Day08.swift
@@ -26,11 +26,31 @@ public class Day08: Day {
     let nodes: [String: Node]
   }
 
+  public struct Walker {
+    let id: Int
+    let map: Map
+    var currentNode: String
+
+    public func step(_ instruction: String) -> String? {
+      switch instruction {
+      case "L":
+        return map.nodes[currentNode]!.left
+
+      case "R":
+        return map.nodes[currentNode]!.right
+
+      default:
+        print("unexpected instruction \(instruction)")
+        return nil
+      }
+    }
+  }
+
   public func part1(_ input: Input) {
     let data = input.asStringArray()
 
-    let mapRL = Day08.parseMap(data)
-    let steps = Day08.walk(mapRL)
+    let map = Day08.parseMap(data)
+    let steps = Day08.walk(map)
 
     print("day 08 part 1: \(steps)")
   }
@@ -38,16 +58,57 @@ public class Day08: Day {
   public func part2(_ input: Input) {
     let data = input.asStringArray()
 
-    print("day 08 part 2: \(data.count)")
+    let map = Day08.parseMap(data)
+    let steps = Day08.walkAll(map)
+
+    print("day 08 part 2: \(steps)")
+  }
+
+  static func gcd(_ a: Int, _ b: Int) -> Int {
+    if a < b {
+      return gcd(b, a)
+    }
+
+    if b == 0 {
+      return a
+    }
+
+    return gcd(b, a % b)
+  }
+
+  static func gcd(_ s: [Int]) -> Int {
+    switch s.count {
+    case 2:
+      return gcd(s.first!, s.last!)
+    case 1:
+      return s.first!
+    default:
+      return gcd(s.first!, gcd(Array(s[1..<s.endIndex])))
+    }
+  }
+
+  static func lcm(_ a: Int, _ b: Int) -> Int {
+    a / gcd(a, b) * b
+  }
+
+  static func lcm(_ s: [Int]) -> Int {
+    switch s.count {
+    case 2:
+      return lcm(s.first!, s.last!)
+    case 1:
+      return s.first!
+    default:
+      return lcm(s.first!, lcm(Array(s[1..<s.endIndex])))
+    }
   }
 
   static func parseMap(_ data: [String]) -> Map {
     let instructions = data[0]
     var nodes: [String: Node] = [:]
 
-    for i in 2..<data.count {
+    for i in 1..<data.count {
       let line = data[i]
-      if let match = line.firstMatch(of: #/([A-Z]+)\s+=\s+\(([A-Z]+),\s+([A-Z]+)\)/#) {
+      if let match = line.firstMatch(of: #/([A-Z0-9]+)\s+=\s+\(([A-Z0-9]+),\s+([A-Z0-9]+)\)/#) {
         let name = String(match.1)
         let left = String(match.2)
         let right = String(match.3)
@@ -59,32 +120,51 @@ public class Day08: Day {
     return Map(instructions: instructions, nodes: nodes)
   }
 
+  static func isStartNode(_ name: String) -> Bool { name.hasSuffix("A") }
+  static func isEndNode(_ name: String) -> Bool { name.hasSuffix("Z") }
+
+  static func startNodes(_ map: Map) -> [String] {
+    Array(map.nodes.keys.filter(isStartNode))
+  }
+
   static func walk(_ map: Map) -> Int {
     var steps = 0
-    var current = "AAA"
+    var walker = Walker(id: 1, map: map, currentNode: "AAA")
 
     for instruction in map.instructions.cycled() {
       steps += 1
-
-      var next: String? = nil
-      switch instruction {
-      case "L":
-        next = map.nodes[current]!.left
-
-      case "R":
-        next = map.nodes[current]!.right
-
-      default:
-        next = nil
+      let nextNode = walker.step(String(instruction))!
+      if nextNode == "ZZZ" {
+        break
       }
-
-      if next == "ZZZ" {
-        return steps
-      }
-
-      current = next!
+      walker.currentNode = nextNode
     }
 
     return steps
+  }
+
+  static func walkAll(_ map: Map) -> Int {
+    var walkers: [Day08.Walker] = Day08.startNodes(map).indexed().map({
+      Walker(id: $0, map: map, currentNode: $1)
+    })
+
+    var cycleLengths: [Int: Int] = [:]
+
+    for i in 0..<walkers.count {
+      var steps = 0
+
+      for instruction in map.instructions.cycled() {
+        steps += 1
+        let nextNode = walkers[i].step(String(instruction))!
+        if isEndNode(nextNode) {
+          break
+        }
+        walkers[i].currentNode = nextNode
+      }
+
+      cycleLengths[walkers[i].id] = steps
+    }
+
+    return lcm(Array(cycleLengths.values))
   }
 }
--- years/2023/swift/Tests/AoCTests/TestDay08.swift
+++ years/2023/swift/Tests/AoCTests/TestDay08.swift
@@ -34,12 +34,48 @@ class TestDay08: XCTestCase {
     "ZZZ = (ZZZ, ZZZ)",
   ]
 
+  let dataPart2 = [
+    "LR",
+    "",
+    "11A = (11B, XXX)",
+    "11B = (XXX, 11Z)",
+    "11Z = (11B, XXX)",
+    "22A = (22B, XXX)",
+    "22B = (22C, 22C)",
+    "22C = (22Z, 22Z)",
+    "22Z = (22B, 22B)",
+    "XXX = (XXX, XXX)",
+  ]
+
+  func testGcd() throws {
+    XCTAssertEqual(Day08.gcd(10, 8), 2)
+    XCTAssertEqual(Day08.gcd(8, 10), 2)
+    XCTAssertEqual(Day08.gcd(3, 5), 1)
+    XCTAssertEqual(Day08.gcd([3]), 3)
+    XCTAssertEqual(Day08.gcd([3, 5]), 1)
+    XCTAssertEqual(Day08.gcd([30, 45, 60]), 15)
+  }
+
+  func testLcm() throws {
+    XCTAssertEqual(Day08.lcm(10, 8), 40)
+    XCTAssertEqual(Day08.lcm(8, 10), 40)
+    XCTAssertEqual(Day08.lcm(3, 5), 15)
+    XCTAssertEqual(Day08.lcm([3]), 3)
+    XCTAssertEqual(Day08.lcm([3, 5]), 15)
+    XCTAssertEqual(Day08.lcm([30, 45, 60]), 180)
+  }
+
   func testParseMap() throws {
     let mapRL = Day08.parseMap(dataRL)
     XCTAssertEqual(mapRL.instructions, "RL")
     XCTAssertEqual(mapRL.nodes.count, 7)
     XCTAssertNotNil(mapRL.nodes["AAA"])
     XCTAssertNil(mapRL.nodes["FOO"])
+  }
+
+  func testStartNodes() throws {
+    let map = Day08.parseMap(dataPart2)
+    XCTAssertEqual(Set(Day08.startNodes(map)), Set(["11A", "22A"]))
   }
 
   func testWalk() throws {
