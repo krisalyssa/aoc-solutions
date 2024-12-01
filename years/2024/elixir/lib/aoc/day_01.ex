55f26e903d1b49e85ca9d339ecb258a330c44162
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-01 00:19:36 -0600

    feat: add code for elixir 2024 day 01 part 1

--- years/2024/elixir/lib/aoc/day_01.ex
+++ years/2024/elixir/lib/aoc/day_01.ex
@@ -9,7 +9,9 @@ defmodule AoC.Day01 do
   def part_1(filename) do
     filename
     |> File.stream!()
-    |> Enum.count()
+    |> parse_lines()
+    |> distances()
+    |> total_distance()
   end
 
   def part_2(filename) do
@@ -17,4 +19,26 @@ defmodule AoC.Day01 do
     |> File.stream!()
     |> Enum.count()
   end
+
+  @spec distances({[integer()], [integer()]}) :: [integer()]
+  def distances({list1, list2}) do
+    with sorted1 <- Enum.sort(list1),
+         sorted2 <- Enum.sort(list2) do
+      Enum.zip_with(sorted1, sorted2, fn item1, item2 -> abs(item1 - item2) end)
+    end
+  end
+
+  @spec parse_lines(Enumerable.t()) :: {[integer()], [integer()]}
+  def parse_lines(data) do
+    Enum.reduce(data, {[], []}, fn line, {list1, list2} ->
+      with str1 <- Regex.scan(~r/(\d+)\s+\d+/, line) |> List.first() |> Enum.at(1),
+           {int1, _} <- Integer.parse(str1),
+           str2 <- Regex.scan(~r/\d+\s+(\d+)/, line) |> List.first() |> Enum.at(1),
+           {int2, _} <- Integer.parse(str2) do
+        {[int1 | list1], [int2 | list2]}
+      end
+    end)
+  end
+
+  def total_distance(list), do: Enum.sum(list)
 end
--- years/2024/elixir/test/aoc/day_01_test.exs
+++ years/2024/elixir/test/aoc/day_01_test.exs
@@ -3,18 +3,73 @@ defmodule AoC.Day01.Test do
 
   use ExUnit.Case, async: false
 
+  alias AoC.Day01
+
   # comment this out to always log to the console
   @moduletag :capture_log
 
   describe "part 1" do
     test "with sample data" do
-      assert AoC.Day01.part_1("../data/01.txt") == 0
+      with temp_dir <- System.tmp_dir!(),
+           temp_file <- "#{temp_dir}/2024-01-sample.txt",
+           :ok <-
+             File.write!(
+               "#{temp_dir}/2024-01-sample.txt",
+               "3   4\n4   3\n2   5\n1   3\n3   9\n3   3"
+             ) do
+        assert AoC.Day01.part_1(temp_file) == 11
+      end
     end
   end
 
   describe "part 2" do
     test "with sample data" do
-      assert AoC.Day01.part_2("../data/01.txt") == 0
+      assert AoC.Day01.part_2("../data/01.txt") == 1000
+    end
+  end
+
+  describe "distances/2" do
+    test "with sample data" do
+      list1 = [3, 3, 1, 2, 4, 3]
+      list2 = [3, 9, 3, 5, 3, 4]
+      distances = Day01.distances({list1, list2})
+
+      refute is_nil(distances)
+      assert is_list(distances)
+      assert Enum.count(distances) == 6
+      assert distances == [2, 1, 0, 1, 2, 5]
+    end
+  end
+
+  describe "total_distance/1" do
+    test "with sample data" do
+      list = [2, 1, 0, 1, 2, 5]
+      assert Day01.total_distance(list) == 11
+    end
+  end
+
+  describe "parse_lines/1" do
+    test "with sample data" do
+      data = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+      ]
+
+      {list1, list2} = Day01.parse_lines(data)
+
+      refute is_nil(list1)
+      assert is_list(list1)
+      assert Enum.count(list1) == 6
+      assert list1 == [3, 3, 1, 2, 4, 3]
+
+      refute is_nil(list2)
+      assert is_list(list2)
+      assert Enum.count(list2) == 6
+      assert list2 == [3, 9, 3, 5, 3, 4]
     end
   end
 end
