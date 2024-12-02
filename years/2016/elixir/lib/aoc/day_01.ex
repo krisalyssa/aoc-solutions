80da144f6a6cae45914303dd732b25e55505195d
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-02 17:04:36 -0600

    feat: add code for elixir 2016 day 01 part 1

--- years/2016/elixir/lib/aoc/day_01.ex
+++ years/2016/elixir/lib/aoc/day_01.ex
@@ -2,19 +2,80 @@ defmodule AoC.Day01 do
   @moduledoc false
 
   def run do
-    IO.puts("day 01 part 1: #{AoC.Day01.part_1("../data/01.txt")}")
-    IO.puts("day 01 part 2: #{AoC.Day01.part_2("../data/01.txt")}")
+    IO.puts("day 01 part 1: #{AoC.Day01.run_part_1("../data/01.txt")}")
+    IO.puts("day 01 part 2: #{AoC.Day01.run_part_2("../data/01.txt")}")
   end
 
-  def part_1(filename) do
+  def run_part_1(filename) do
     filename
     |> File.stream!()
-    |> Enum.count()
+    |> part_1()
   end
 
-  def part_2(filename) do
+  def run_part_2(filename) do
     filename
     |> File.stream!()
+    |> part_2()
+  end
+
+  @spec part_1(Enumerable.t()) :: integer() | String.t()
+  def part_1(data) do
+    {_, coordinates} =
+      data
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.split(&1, ~r/[,\s]+/))
+      |> List.flatten()
+      |> Enum.reduce({:north, {0, 0}}, fn instruction, {heading, coordinates} ->
+        {new_heading, new_coordinates} =
+          case instruction do
+            "L" <> steps ->
+              new_heading =
+                case heading do
+                  :north -> :west
+                  :east -> :north
+                  :south -> :east
+                  :west -> :south
+                end
+
+              {new_heading, move(coordinates, new_heading, steps)}
+
+            "R" <> steps ->
+              new_heading =
+                case heading do
+                  :north -> :east
+                  :east -> :south
+                  :south -> :west
+                  :west -> :north
+                end
+
+              {new_heading, move(coordinates, new_heading, steps)}
+          end
+
+        {new_heading, new_coordinates}
+      end)
+
+    distance(coordinates)
+  end
+
+  @spec distance({integer(), integer()}) :: integer()
+  def distance({x, y}), do: abs(x) + abs(y)
+
+  @spec move({integer(), integer()}, :north | :east | :south | :west, String.t()) ::
+          {integer(), integer()}
+  def move({x, y}, heading, steps_str) do
+    {steps, _} = Integer.parse(steps_str)
+
+    case heading do
+      :north -> {x, y + steps}
+      :east -> {x + steps, y}
+      :south -> {x, y - steps}
+      :west -> {x - steps, y}
+    end
+  end
+
+  @spec part_2(Enumerable.t()) :: integer() | String.t()
+  def part_2(data) do
+    data
     |> Enum.count()
   end
 end
--- years/2016/elixir/test/aoc/day_01_test.exs
+++ years/2016/elixir/test/aoc/day_01_test.exs
@@ -3,18 +3,36 @@ defmodule AoC.Day01.Test do
 
   use ExUnit.Case, async: false
 
+  alias AoC.Day01
+
   # comment this out to always log to the console
   @moduletag :capture_log
 
   describe "part 1" do
     test "with sample data" do
-      assert AoC.Day01.part_1("../data/01.txt") == 0
+      assert Day01.part_1(["R2, L3"]) == 5
+      assert Day01.part_1(["R2, R2, R2"]) == 2
+      assert Day01.part_1(["R5, L5, R5, R3"]) == 12
     end
   end
 
   describe "part 2" do
     test "with sample data" do
-      assert AoC.Day01.part_2("../data/01.txt") == 0
+      assert Day01.part_2(["../data/01.txt"]) == 1
+    end
+  end
+
+  describe "distance/1" do
+    test "with sample data" do
+      assert Day01.distance({2, 3}) == 5
+      assert Day01.distance({0, -2}) == 2
+    end
+  end
+
+  describe "move/3" do
+    test "with sample data" do
+      assert Day01.move({0, 0}, :east, "2") == {2, 0}
+      assert Day01.move({2, 0}, :north, "3") == {2, 3}
     end
   end
 end
