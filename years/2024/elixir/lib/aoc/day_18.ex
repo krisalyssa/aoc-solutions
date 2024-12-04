a1246fb64a1d9b5f43bd75fa7ccb0a36b66f3585
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-04 07:50:30 -0600

    chore: update test files from skeleton

--- years/2024/elixir/lib/aoc/day_18.ex
+++ years/2024/elixir/lib/aoc/day_18.ex
@@ -1,20 +1,35 @@
 defmodule AoC.Day18 do
   @moduledoc false
 
+  @spec run() :: :ok
   def run do
-    IO.puts("day 18 part 1: #{AoC.Day18.part_1("../data/18.txt")}")
-    IO.puts("day 18 part 2: #{AoC.Day18.part_2("../data/18.txt")}")
+    IO.puts("day 18 part 1: #{AoC.Day18.run_part_1("../data/18.txt")}")
+    IO.puts("day 18 part 2: #{AoC.Day18.run_part_2("../data/18.txt")}")
   end
 
-  def part_1(filename) do
+  @spec run_part_1(String.t()) :: number()
+  def run_part_1(filename) do
     filename
     |> File.stream!()
+    |> part_1()
+  end
+
+  @spec run_part_2(String.t()) :: number()
+  def run_part_2(filename) do
+    filename
+    |> File.stream!()
+    |> part_2()
+  end
+
+  @spec part_1(Enumerable.t()) :: integer()
+  def part_1(data) do
+    data
     |> Enum.count()
   end
 
-  def part_2(filename) do
-    filename
-    |> File.stream!()
+  @spec part_2(Enumerable.t()) :: integer()
+  def part_2(data) do
+    data
     |> Enum.count()
   end
 end
