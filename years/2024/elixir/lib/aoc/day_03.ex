b7ff980c9317df2e1b92853ee776f3183b53f0dd
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-03 19:48:04 -0600

    feat: add code for elixir 2024 day 03 part 1

--- years/2024/elixir/lib/aoc/day_03.ex
+++ years/2024/elixir/lib/aoc/day_03.ex
@@ -1,20 +1,48 @@
 defmodule AoC.Day03 do
   @moduledoc false
 
+  @spec run() :: :ok
   def run do
-    IO.puts("day 03 part 1: #{AoC.Day03.part_1("../data/03.txt")}")
-    IO.puts("day 03 part 2: #{AoC.Day03.part_2("../data/03.txt")}")
+    IO.puts("day 03 part 1: #{AoC.Day03.run_part_1("../data/03.txt")}")
+    IO.puts("day 03 part 2: #{AoC.Day03.run_part_2("../data/03.txt")}")
   end
 
-  def part_1(filename) do
+  @spec run_part_1(String.t()) :: number()
+  def run_part_1(filename) do
     filename
     |> File.stream!()
+    |> part_1()
+  end
+
+  @spec run_part_2(String.t()) :: number()
+  def run_part_2(filename) do
+    filename
+    |> File.stream!()
+    |> part_2()
+  end
+
+  @spec part_1(Enumerable.t()) :: integer()
+  def part_1(data) do
+    data
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&extract_instructions/1)
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  @spec part_2(Enumerable.t()) :: integer()
+  def part_2(data) do
+    data
     |> Enum.count()
   end
 
-  def part_2(filename) do
-    filename
-    |> File.stream!()
-    |> Enum.count()
+  defp extract_instructions(str) do
+    Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, str)
+    |> Enum.map(fn [_, a_str, b_str] ->
+      {a, _} = Integer.parse(a_str)
+      {b, _} = Integer.parse(b_str)
+      {a, b}
+    end)
+    |> Enum.reduce([], fn {a, b}, acc -> [a * b | acc] end)
   end
 end
--- years/2024/elixir/test/aoc/day_03_test.exs
+++ years/2024/elixir/test/aoc/day_03_test.exs
@@ -8,13 +8,15 @@ defmodule AoC.Day03.Test do
 
   describe "part 1" do
     test "with sample data" do
-      assert AoC.Day03.part_1("../data/03.txt") == 0
+      assert AoC.Day03.part_1([
+               "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+             ]) == 161
     end
   end
 
   describe "part 2" do
     test "with sample data" do
-      assert AoC.Day03.part_2("../data/03.txt") == 0
+      assert AoC.Day03.part_2(["../data/03.txt"]) == 1
     end
   end
 end
