784677d6e585593c1883991921eb6b1aa24ae819
Kris Cottingham <krisalyssa@cottingham.net>
2024-12-05 22:14:58 -0600

    feat: add code for elixir 2024 day 05 part 1

--- years/2024/elixir/lib/aoc/day_05.ex
+++ years/2024/elixir/lib/aoc/day_05.ex
@@ -23,13 +23,53 @@ defmodule AoC.Day05 do
 
   @spec part_1(Enumerable.t()) :: integer()
   def part_1(data) do
-    data
-    |> Enum.count()
+    {rules, updates} = parse_data(data)
+
+    updates
+    |> Enum.filter(&valid?(&1, rules))
+    |> Enum.map(&middle_page_number/1)
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
   end
 
   @spec part_2(Enumerable.t()) :: integer()
   def part_2(data) do
     data
     |> Enum.count()
+  end
+
+  def correct_order?({page1, page2}, rules) do
+    if Enum.any?(rules, fn [a, b] -> page1 == b && page2 == a end) do
+      false
+    else
+      true
+    end
+  end
+
+  def middle_page_number(list), do: list |> AoC.bisect() |> elem(1) |> List.first()
+
+  def parse_data(data) do
+    {rules, updates} =
+      data
+      |> Enum.map(&String.trim/1)
+      |> Enum.split_while(fn line -> String.length(line) > 0 end)
+
+    {Enum.map(rules, &String.split(&1, "|")),
+     updates
+     |> Enum.filter(fn line -> String.length(line) > 0 end)
+     |> Enum.map(&String.split(&1, ","))}
+  end
+
+  def valid?([], _) do
+    true
+  end
+
+  def valid?([page1 | rest], rules) do
+    if Enum.all?(rest, &correct_order?({page1, &1}, rules)) do
+      valid?(rest, rules)
+    else
+      false
+    end
   end
 end
--- years/2024/elixir/test/aoc/day_05_test.exs
+++ years/2024/elixir/test/aoc/day_05_test.exs
@@ -8,15 +8,128 @@ defmodule AoC.Day05.Test do
   # comment this out to always log to the console
   @moduletag :capture_log
 
+  @sample_data ~S"""
+  47|53
+  97|13
+  97|61
+  97|47
+  75|29
+  61|13
+  75|53
+  29|13
+  97|29
+  53|29
+  61|53
+  97|53
+  61|29
+  47|13
+  75|47
+  97|75
+  47|61
+  75|61
+  47|29
+  75|13
+  53|13
+
+  75,47,61,53,29
+  97,61,53,29,13
+  75,29,13
+  75,97,47,61,53
+  61,13,29
+  97,13,75,29,47
+  """
+
   describe "part 1" do
     test "with sample data" do
-      assert Day05.part_1(["../data/05.txt"]) == 1
+      assert Day05.part_1(@sample_data |> String.split("\n") |> Enum.drop(1)) == 143
     end
   end
 
   describe "part 2" do
     test "with sample data" do
       assert Day05.part_2(["../data/05.txt"]) == 1
+    end
+  end
+
+  describe "correct_order/2" do
+    test "with sample data" do
+      {rules, _} = @sample_data |> String.split("\n") |> Enum.drop(1) |> Day05.parse_data()
+
+      # ["75", "47", "61", "53", "29"]
+      assert Day05.correct_order?({"75", "47"}, rules) == true
+      assert Day05.correct_order?({"75", "61"}, rules) == true
+      assert Day05.correct_order?({"75", "53"}, rules) == true
+      assert Day05.correct_order?({"75", "29"}, rules) == true
+
+      # ["75", "97", "47", "61", "53"]
+      assert Day05.correct_order?({"75", "97"}, rules) == false
+
+      # ["61", "13", "29"]
+      assert Day05.correct_order?({"61", "13"}, rules) == true
+      assert Day05.correct_order?({"61", "29"}, rules) == true
+      assert Day05.correct_order?({"13", "29"}, rules) == false
+    end
+  end
+
+  describe "middle_page_number/1" do
+    test "with sample data" do
+      assert Day05.middle_page_number(["75", "47", "61", "53", "29"]) == "61"
+      assert Day05.middle_page_number(["97", "61", "53", "29", "13"]) == "53"
+      assert Day05.middle_page_number(["75", "29", "13"]) == "29"
+      assert Day05.middle_page_number(["75", "97", "47", "61", "53"]) == "47"
+      assert Day05.middle_page_number(["61", "13", "29"]) == "13"
+      assert Day05.middle_page_number(["97", "13", "75", "29", "47"]) == "75"
+    end
+  end
+
+  describe "parse_data/1" do
+    test "with sample data" do
+      # trim_leading/1 because there's a newline at the beginning
+      assert Day05.parse_data(@sample_data |> String.split("\n") |> Enum.drop(1)) == {
+               [
+                 ["97", "13"],
+                 ["97", "61"],
+                 ["97", "47"],
+                 ["75", "29"],
+                 ["61", "13"],
+                 ["75", "53"],
+                 ["29", "13"],
+                 ["97", "29"],
+                 ["53", "29"],
+                 ["61", "53"],
+                 ["97", "53"],
+                 ["61", "29"],
+                 ["47", "13"],
+                 ["75", "47"],
+                 ["97", "75"],
+                 ["47", "61"],
+                 ["75", "61"],
+                 ["47", "29"],
+                 ["75", "13"],
+                 ["53", "13"]
+               ],
+               [
+                 ["75", "47", "61", "53", "29"],
+                 ["97", "61", "53", "29", "13"],
+                 ["75", "29", "13"],
+                 ["75", "97", "47", "61", "53"],
+                 ["61", "13", "29"],
+                 ["97", "13", "75", "29", "47"]
+               ]
+             }
+    end
+  end
+
+  describe "valid?/2" do
+    test "with sample data" do
+      {rules, _} = @sample_data |> String.split("\n") |> Enum.drop(1) |> Day05.parse_data()
+
+      assert Day05.valid?(["75", "47", "61", "53", "29"], rules) == true
+      assert Day05.valid?(["97", "61", "53", "29", "13"], rules) == true
+      assert Day05.valid?(["75", "29", "13"], rules) == true
+      assert Day05.valid?(["75", "97", "47", "61", "53"], rules) == false
+      assert Day05.valid?(["61", "13", "29"], rules) == false
+      assert Day05.valid?(["97", "13", "75", "29", "47"], rules) == false
     end
   end
 end
