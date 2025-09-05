8a06300a6d2baa495fe50ad3b810d6f179e7b00d
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-05 08:26:07 -0500

    feat: add code for elixir 2015 day 05 part 1

--- years/2015/elixir/lib/aoc/day_05.ex
+++ years/2015/elixir/lib/aoc/day_05.ex
@@ -29,6 +29,7 @@ defmodule AoC.Day05 do
   @spec part_1(Enumerable.t()) :: integer()
   def part_1(data) do
     data
+    |> Enum.filter(&nice?/1)
     |> Enum.count()
   end
 
@@ -36,5 +37,36 @@ defmodule AoC.Day05 do
   def part_2(data) do
     data
     |> Enum.count()
+  end
+
+  def at_least_three_vowels?(str) do
+    ~r/[aeiou]/
+    |> Regex.scan(str)
+    |> Enum.count()
+    |> then(fn v -> v >= 3 end)
+  end
+
+  def doubled_letters?(str) do
+    ~r/(.)\1/
+    |> Regex.scan(str)
+    |> Enum.count()
+    |> then(fn v -> v > 0 end)
+  end
+
+  def nice?(str) do
+    with true <- at_least_three_vowels?(str),
+         true <- doubled_letters?(str),
+         true <- no_forbidden_strings?(str) do
+      true
+    else
+      _ -> false
+    end
+  end
+
+  def no_forbidden_strings?(str) do
+    ~r/ab|cd|pq|xy/
+    |> Regex.scan(str)
+    |> Enum.count()
+    |> then(fn v -> v == 0 end)
   end
 end
--- years/2015/elixir/test/aoc/day_05_test.exs
+++ years/2015/elixir/test/aoc/day_05_test.exs
@@ -8,15 +8,62 @@ defmodule AoC.Day05.Test do
   # comment this out to always log to the console
   @moduletag :capture_log
 
-  describe "part 1" do
-    test "with sample data" do
-      assert Day05.part_1(["../data/05.txt"]) == 1
-    end
-  end
+  # describe "part 1" do
+  #   test "with sample data" do
+  #   end
+  # end
 
   describe "part 2" do
     test "with sample data" do
       assert Day05.part_2(["../data/05.txt"]) == 1
+    end
+  end
+
+  describe "at_least_three_vowels?/1" do
+    test "with sample data" do
+      assert Day05.at_least_three_vowels?("aei")
+      assert Day05.at_least_three_vowels?("xazegov")
+      assert Day05.at_least_three_vowels?("aeiouaeiouaeiou")
+
+      assert Day05.at_least_three_vowels?("ugknbfddgicrmopn")
+      assert Day05.at_least_three_vowels?("aaa")
+      assert Day05.at_least_three_vowels?("jchzalrnumimnmhp")
+      assert Day05.at_least_three_vowels?("haegwjzuvuyypxyu")
+      refute Day05.at_least_three_vowels?("dvszwmarrgswjxmb")
+    end
+  end
+
+  describe "doubled_letters?/1" do
+    test "with sample data" do
+      assert Day05.doubled_letters?("xx")
+      assert Day05.doubled_letters?("abcdde")
+      assert Day05.doubled_letters?("aabbccdd")
+
+      assert Day05.doubled_letters?("ugknbfddgicrmopn")
+      assert Day05.doubled_letters?("aaa")
+      refute Day05.doubled_letters?("jchzalrnumimnmhp")
+      assert Day05.doubled_letters?("haegwjzuvuyypxyu")
+      assert Day05.doubled_letters?("dvszwmarrgswjxmb")
+    end
+  end
+
+  describe "nice?/1" do
+    test "with sample data" do
+      assert Day05.nice?("ugknbfddgicrmopn")
+      assert Day05.nice?("aaa")
+      refute Day05.nice?("jchzalrnumimnmhp")
+      refute Day05.nice?("haegwjzuvuyypxyu")
+      refute Day05.nice?("dvszwmarrgswjxmb")
+    end
+  end
+
+  describe "no_forbidden_strings?/1" do
+    test "with sample data" do
+      assert Day05.no_forbidden_strings?("ugknbfddgicrmopn")
+      assert Day05.no_forbidden_strings?("aaa")
+      assert Day05.no_forbidden_strings?("jchzalrnumimnmhp")
+      refute Day05.no_forbidden_strings?("haegwjzuvuyypxyu")
+      assert Day05.no_forbidden_strings?("dvszwmarrgswjxmb")
     end
   end
 end
