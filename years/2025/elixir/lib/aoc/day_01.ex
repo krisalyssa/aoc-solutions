57c60a220d68a4928cce334ffb78c901a9a51ce8
Kris Cottingham <kris.cottingham@redfin.com>
2025-12-04 23:06:22 -0600

    feat: add code for elixir 2025 day 01 part 1

--- years/2025/elixir/lib/aoc/day_01.ex
+++ years/2025/elixir/lib/aoc/day_01.ex
@@ -6,7 +6,7 @@ defmodule AoC.Day01 do
     do:
       IO.puts(
         AoC.print(
-          2,
+          1,
           AoC.Day01.run_part_1("../data/01.txt"),
           AoC.Day01.run_part_2("../data/01.txt")
         )
@@ -28,13 +28,51 @@ defmodule AoC.Day01 do
 
   @spec part_1(Enumerable.t()) :: integer()
   def part_1(data) do
+    state = {50, 0}
+
     data
-    |> Enum.count()
+    |> Enum.map(&parse_instruction/1)
+    |> Enum.reduce(state, &spin/2)
+    |> elem(1)
   end
 
   @spec part_2(Enumerable.t()) :: integer()
   def part_2(data) do
     data
     |> Enum.count()
+  end
+
+  @spec one_if_zero(position :: integer()) :: non_neg_integer()
+  def one_if_zero(0), do: 1
+  def one_if_zero(_), do: 0
+
+  @spec parse_instruction(String.t()) ::
+          {:ok, {:left | :right, non_neg_integer()}} | {:error, any()}
+  def parse_instruction(str) do
+    case Regex.run(~r/([LR])(\d+)/, str) do
+      [_, "L", count] ->
+        {:ok, {:left, String.to_integer(count)}}
+
+      [_, "R", count] ->
+        {:ok, {:right, String.to_integer(count)}}
+
+      _ ->
+        {:error, str}
+    end
+  end
+
+  @spec spin(
+          instruction :: {:ok, {:left | :right, non_neg_integer()}} | {:error, any()},
+          state :: {integer(), non_neg_integer()}
+        ) :: {integer(), non_neg_integer()}
+
+  def spin({:ok, {:left, count}}, {position, zeros}) do
+    new_position = position - count
+    {new_position, zeros + one_if_zero(Integer.mod(new_position, 100))}
+  end
+
+  def spin({:ok, {:right, count}}, {position, zeros}) do
+    new_position = position + count
+    {new_position, zeros + one_if_zero(Integer.mod(new_position, 100))}
   end
 end
--- years/2025/elixir/test/aoc/day_01_test.exs
+++ years/2025/elixir/test/aoc/day_01_test.exs
@@ -10,13 +10,54 @@ defmodule AoC.Day01.Test do
 
   describe "part 1" do
     test "with sample data" do
-      assert Day01.part_1(["../data/01.txt"]) == 1
+      data = [
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"
+      ]
+
+      assert Day01.part_1(data) == 3
     end
   end
 
   describe "part 2" do
     test "with sample data" do
-      assert Day01.part_2(["../data/01.txt"]) == 1
+      data = [
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"
+      ]
+
+      assert Day01.part_2(data) == 10
+    end
+  end
+
+  describe "parse_instruction/1" do
+    test "with sample data" do
+      assert Day01.parse_instruction("L68") == {:ok, {:left, 68}}
+      assert Day01.parse_instruction("R5") == {:ok, {:right, 5}}
+      assert Day01.parse_instruction("") == {:error, ""}
+    end
+  end
+
+  describe "spin/2" do
+    test "with sample data" do
+      assert Day01.spin({:ok, {:right, 8}}, {11, 0}) == {19, 0}
+      assert Day01.spin({:ok, {:left, 19}}, {19, 0}) == {0, 1}
     end
   end
 end
