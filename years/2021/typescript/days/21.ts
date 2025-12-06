6cbc4ad03e4e835d11eea7d759738dc939cd8ee8
Kris Cottingham <kris.cottingham@redfin.com>
2025-12-05 21:47:41 -0600

    chore: add skeleton files for remaining days in 2021

---
+++ years/2021/typescript/days/21.ts
@@ -0,0 +1,13 @@
+export async function run(input: string) {
+  return [await runPart1(input), await runPart2(input)]
+}
+
+export async function runPart1(input: string) {
+  console.log("inside day 21 part 1")
+  return undefined
+}
+
+export async function runPart2(input: string) {
+  console.log("inside day 21 part 2")
+  return undefined
+}
