f3cd44bd48b036399e84ab14b27cf98e919b5784
Kris Cottingham <kris.cottingham@redfin.com>
2025-12-05 21:16:56 -0600

    feat: add code for typescript 2021 day 14

--- years/2021/typescript/days/14.ts
+++ years/2021/typescript/days/14.ts
@@ -1,22 +1,15 @@
-import chalk from "chalk";
-import { performance } from "perf_hooks";
-import { log, logSolution } from "../util/log";
-import * as test from "../util/test";
-import * as util from "../util/util";
+import * as util from "../util";
 
-const YEAR = 2021;
-const DAY = 14;
-
-// solution path: /Users/ccottingham/Projects/advent-of-code/2021/years/2021/14/index.ts
-// data path    : /Users/ccottingham/Projects/advent-of-code/2021/years/2021/14/data.txt
-// problem url  : https://adventofcode.com/2021/day/14
-
-async function p2021day14_part1(input: string, ...params: any[]) {
-	return polymerize(input, params[0] || 10);
+export async function run(input: string) {
+  return [await runPart1(input), await runPart2(input)]
 }
 
-async function p2021day14_part2(input: string, ...params: any[]) {
-	return polymerize(input, params[0] || 40);
+export async function runPart1(input: string) {
+	return polymerize(input, 10);
+}
+
+export async function runPart2(input: string) {
+	return polymerize(input, 40);
 }
 
 /**
@@ -209,78 +202,3 @@ function polymerize(input: string, iterations: number): number {
 
 	return maxCount - minCount;
 }
-
-async function run() {
-	const testData = `
-NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C
-	`;
-	const part1tests: TestCase[] = [
-		{
-			input: testData,
-			expected: "1588",
-		},
-	];
-	const part2tests: TestCase[] = [
-		{
-			input: testData,
-			expected: "2188189693529",
-		},
-	];
-
-	// Run tests
-	test.beginTests();
-	await test.section(async () => {
-		for (const testCase of part1tests) {
-			test.logTestResult(testCase, String(await p2021day14_part1(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	await test.section(async () => {
-		for (const testCase of part2tests) {
-			test.logTestResult(testCase, String(await p2021day14_part2(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	test.endTests();
-
-	// Get input and run program while measuring performance
-	const input = await util.getInput(DAY, YEAR);
-
-	const part1Before = performance.now();
-	const part1Solution = String(await p2021day14_part1(input));
-	const part1After = performance.now();
-
-	const part2Before = performance.now();
-	const part2Solution = String(await p2021day14_part2(input));
-	const part2After = performance.now();
-
-	logSolution(14, 2021, part1Solution, part2Solution);
-
-	log(chalk.gray("--- Performance ---"));
-	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
-	log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
-	log();
-}
-
-run()
-	.then(() => {
-		process.exit();
-	})
-	.catch(error => {
-		throw error;
-	});
