1a372a4096d1c7112b658e21efabbe04d34460b2
Kris Cottingham <kris.cottingham@redfin.com>
2025-12-06 15:14:06 -0600

    feat: add code for elixir 2025 day 03 part 1

--- years/2025/elixir/lib/aoc/day_03.ex
+++ years/2025/elixir/lib/aoc/day_03.ex
@@ -2,7 +2,15 @@ defmodule AoC.Day03 do
   @moduledoc false
 
   @spec run() :: :ok
-  def run, do: IO.puts(AoC.print(3, AoC.Day03.run_part_1("../data/03.txt"), AoC.Day03.run_part_2("../data/03.txt")))
+  def run,
+    do:
+      IO.puts(
+        AoC.print(
+          3,
+          AoC.Day03.run_part_1("../data/03.txt"),
+          AoC.Day03.run_part_2("../data/03.txt")
+        )
+      )
 
   @spec run_part_1(String.t()) :: number()
   def run_part_1(filename) do
@@ -19,14 +27,39 @@ defmodule AoC.Day03 do
   end
 
   @spec part_1(Enumerable.t()) :: integer()
-  def part_1(data) do
-    data
-    |> Enum.count()
-  end
+  def part_1(data),
+    do: Enum.reduce(data, 0, fn p, acc -> acc + String.to_integer(max_pair_in_bank(p)) end)
 
   @spec part_2(Enumerable.t()) :: integer()
   def part_2(data) do
     data
     |> Enum.count()
   end
+
+  @spec max_pair_in_bank(String.t()) :: String.t()
+  def max_pair_in_bank(bank) do
+    bank
+    |> String.trim()
+    |> subbanks()
+    |> Enum.reject(fn b -> String.length(b) < 2 end)
+    |> Enum.map(&max_pair_in_subbank/1)
+    |> Enum.max_by(&String.to_integer/1)
+  end
+
+  @spec max_pair_in_subbank(String.t()) :: String.t()
+  def max_pair_in_subbank(subbank) do
+    subbank
+    |> pairs_with_first()
+    |> Enum.max_by(&String.to_integer/1)
+  end
+
+  @spec pairs_with_first(String.t()) :: [integer]
+  def pairs_with_first(bank) do
+    [left | all_right] = String.graphemes(bank)
+    Enum.reduce(all_right, [], fn r, acc -> [left <> r | acc] end)
+  end
+
+  @spec subbanks(String.t()) :: [String.t()]
+  def subbanks(""), do: []
+  def subbanks(bank), do: [bank | subbanks(String.slice(bank, 1..-1//1))]
 end
--- years/2025/elixir/test/aoc/day_03_test.exs
+++ years/2025/elixir/test/aoc/day_03_test.exs
@@ -10,13 +10,79 @@ defmodule AoC.Day03.Test do
 
   describe "part 1" do
     test "with sample data" do
-      assert Day03.part_1(["../data/03.txt"]) == 1
+      assert Day03.part_1([
+               "987654321111111",
+               "811111111111119",
+               "234234234234278",
+               "818181911112111"
+             ]) == 357
     end
   end
 
   describe "part 2" do
     test "with sample data" do
       assert Day03.part_2(["../data/03.txt"]) == 1
+    end
+  end
+
+  describe "max_pair_in_bank/1" do
+    test "with sample data" do
+      assert Day03.max_pair_in_bank("987654321111111") == "98"
+      assert Day03.max_pair_in_bank("811111111111119") == "89"
+      assert Day03.max_pair_in_bank("234234234234278") == "78"
+      assert Day03.max_pair_in_bank("818181911112111") == "92"
+    end
+  end
+
+  describe "max_pair_in_subbank/1" do
+    test "with sample data" do
+      assert Day03.max_pair_in_subbank("987654321111111") == "98"
+      assert Day03.max_pair_in_subbank("811111111111119") == "89"
+      assert Day03.max_pair_in_subbank("234234234234278") == "28"
+      assert Day03.max_pair_in_subbank("818181911112111") == "89"
+    end
+  end
+
+  describe "pairs_with_first/1" do
+    test "with sample data" do
+      assert Day03.pairs_with_first("987654321111111") == [
+               "91",
+               "91",
+               "91",
+               "91",
+               "91",
+               "91",
+               "91",
+               "92",
+               "93",
+               "94",
+               "95",
+               "96",
+               "97",
+               "98"
+             ]
+    end
+  end
+
+  describe "subbanks/1" do
+    test "with sample data" do
+      assert Day03.subbanks("987654321111111") == [
+               "987654321111111",
+               "87654321111111",
+               "7654321111111",
+               "654321111111",
+               "54321111111",
+               "4321111111",
+               "321111111",
+               "21111111",
+               "1111111",
+               "111111",
+               "11111",
+               "1111",
+               "111",
+               "11",
+               "1"
+             ]
     end
   end
 end
