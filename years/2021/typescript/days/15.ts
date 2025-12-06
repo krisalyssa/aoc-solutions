eb9a7d144a720ed2ac003d71d5b2436d4b302163
Kris Cottingham <kris.cottingham@redfin.com>
2025-12-05 21:40:37 -0600

    feat: add code for typescript 2021 day 15

--- years/2021/typescript/days/15.ts
+++ years/2021/typescript/days/15.ts
@@ -1,19 +1,11 @@
-import aStar from "a-star";
-import chalk from "chalk";
-import { performance } from "perf_hooks";
-import { Grid, GridPos } from "../util/grid";
-import { log, logSolution } from "../util/log";
-import * as test from "../util/test";
-import * as util from "../util/util";
+import aStar from "a-star"
+import { Grid, GridPos } from "../grid"
 
-const YEAR = 2021;
-const DAY = 15;
+export async function run(input: string) {
+  return [await runPart1(input), await runPart2(input)]
+}
 
-// solution path: /Users/ccottingham/Projects/advent-of-code/2021/years/2021/15/index.ts
-// data path    : /Users/ccottingham/Projects/advent-of-code/2021/years/2021/15/data.txt
-// problem url  : https://adventofcode.com/2021/day/15
-
-async function p2021day15_part1(input: string, ...params: any[]) {
+export async function runPart1(input: string) {
 	const grid = new Grid({ serialized: input.trim() });
 
 	const result = aStar({
@@ -50,7 +42,7 @@ async function p2021day15_part1(input: string, ...params: any[]) {
 // they are.
 // So just brute-force it, and generate the full map.
 
-async function p2021day15_part2(input: string, ...params: any[]) {
+export async function runPart2(input: string) {
 	const tile = new Grid({ serialized: input.trim() });
 	const grid = new Grid({ rowCount: tile.rowCount * 5, colCount: tile.colCount * 5 });
 
@@ -125,70 +117,3 @@ function pathRisk(grid: Grid, path: number[][]): number {
 function tiledValue(tileRow: number, tileCol: number, value: string): string {
 	return (((Number(value) - 1 + tileRow + tileCol) % 9) + 1).toString();
 }
-
-async function run() {
-	const testData = `
-1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581
-	`;
-	const part1tests: TestCase[] = [
-		{
-			input: testData,
-			expected: "40",
-		},
-	];
-	const part2tests: TestCase[] = [
-		{
-			input: testData,
-			expected: "315",
-		},
-	];
-
-	// Run tests
-	test.beginTests();
-	await test.section(async () => {
-		for (const testCase of part1tests) {
-			test.logTestResult(testCase, String(await p2021day15_part1(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	await test.section(async () => {
-		for (const testCase of part2tests) {
-			test.logTestResult(testCase, String(await p2021day15_part2(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	test.endTests();
-
-	// Get input and run program while measuring performance
-	const input = await util.getInput(DAY, YEAR);
-
-	const part1Before = performance.now();
-	const part1Solution = String(await p2021day15_part1(input));
-	const part1After = performance.now();
-
-	const part2Before = performance.now();
-	const part2Solution = String(await p2021day15_part2(input));
-	const part2After = performance.now();
-
-	logSolution(15, 2021, part1Solution, part2Solution);
-
-	log(chalk.gray("--- Performance ---"));
-	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
-	log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
-	log();
-}
-
-run()
-	.then(() => {
-		process.exit();
-	})
-	.catch(error => {
-		throw error;
-	});
